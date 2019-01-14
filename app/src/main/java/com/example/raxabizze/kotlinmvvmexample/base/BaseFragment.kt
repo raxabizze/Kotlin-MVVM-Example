d1974f9c0dca07bfef9fa311fdd0bd6530f266a7
raxabizze <fusenhanj5@gmail.com>
2019-01-14 11:27:19 +0800

    fix fragment

--- app/src/main/java/com/example/raxabizze/kotlinmvvmexample/base/BaseFragment.kt
+++ app/src/main/java/com/example/raxabizze/kotlinmvvmexample/base/BaseFragment.kt
@@ -1,24 +1,18 @@
 package com.example.raxabizze.kotlinmvvmexample.base
 
 import android.support.annotation.StringRes
+import android.widget.Toast
 import dagger.android.support.DaggerFragment
 
 
 abstract class BaseFragment : BaseView, DaggerFragment() {
 
-    override fun showProgressDialog(@StringRes text: Int) {
-
+    override fun showToast(@StringRes text: Int) {
+        Toast.makeText(activity, text, Toast.LENGTH_SHORT).show()
     }
 
-    override fun dismissProgressDialog() {
+    override fun showToast(text: String) {
+        Toast.makeText(activity, text, Toast.LENGTH_SHORT).show()
     }
-
-//    override fun showToast(@StringRes text: Int) {
-////        Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
-//    }
-//
-//    override fun showToast(text: String) {
-////        Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
-//    }
 
 }
